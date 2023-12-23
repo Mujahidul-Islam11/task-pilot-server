6ec59157b41795f87ecc396573401626e8e72b58
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-12-23 13:02:14 +0600

    Server site crud oparetion done

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const taskCollection = client.db("pilot").collection("tasks");
     app.get('/', (req, res)=>{
         res.send('task pilot is running')
@@ -44,15 +44,73 @@ async function run() {
         const result = await taskCollection.deleteOne(query)
         res.send(result)
     })
-    app.patch("/taskEd/:id", async(req, res)=>{
-        const id = req.params
-        const query = {_id : new ObjectId(id)}
-        const result = await taskCollection.updateOne(query)
-        res.send(result)
-    })
     
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/task/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/tasks/item/:id", async (request, response) => {
+      const id = request.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      response.status(200).send(result);
+    });
+    app.patch("/task", async (req, res) => {
+      const id = req.query.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      try {
+        const result = await taskCollection.deleteOne(query);
+        if (result.deletedCount === 1) {
+          res.status(200).json({ message: "Task deleted successfully" });
+        } else {
+          res.status(404).json({ message: "Task not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+    app.get("/updatetask/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/taskEd/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          title: item.title,
+          deadline: item.deadline,
+          description: item.description,
+          priority: item.priority,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
